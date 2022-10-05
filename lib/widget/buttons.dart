a89d32d2553592cf7990058bf510954ce14008d2
10YUG <yuj192001@gmail.com>
2022-10-05 23:35:24 +0530

    PaymentApp

---
+++ lib/widget/buttons.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_payment_app/component/colors.dart';
+
+class AppButtons extends StatelessWidget {
+  final double? fontSize;
+  final IconData icon;
+  final Function()? onTap;
+  final Color backGroundColor;
+  final Color? iconColor;
+  final Color? textColor;
+  final String? text;
+  
+  const AppButtons({Key? key,
+  this.fontSize = 20,
+  this.textColor = AppColor.mainColor,
+  this.iconColor = Colors.white,
+  this.backGroundColor = AppColor.mainColor,
+  required this.icon,
+  this.text,
+  this.onTap,
+
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: backGroundColor,
+            ),
+            child: Icon(icon,size: 30,color: iconColor,),
+          ),
+          text!=null?Text(
+            text!,
+            style:TextStyle(
+              fontSize:14,
+              color: textColor
+            )
+          ):Container()
+        ],
+      ),
+    );
+  }
+}
